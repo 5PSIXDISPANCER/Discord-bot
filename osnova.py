52159f35fad89aa0f1facac3f05f186c9a4e96b0
Ben-Beee <yepupuyp@mail.ru>
2024-04-24 20:36:59 +0300

    a

--- osnova.py
+++ osnova.py
@@ -1,25 +1,24 @@
 import discord
+import config
 from discord.ext import commands
 from discord.utils import get
 
-TOKEN = 'MTIzMjQwODgwNTkzNDEwODg4NQ.GlSw9Z.l2oB37atqjZN9uMEUman1DJ-rb4rbB91QpfSiA'
-PREFIX = '!'
 intents = discord.Intents().all()
 
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix=config.prefix, intents=intents)
 
 @bot.command()
 async def ping(stx):
     await stx.send('pong')
-111
 @bot.command()
 async def poshel(stx):
     await stx.send('nahuy')
 
 @bot.command()
-async def cc(ctx):
+async def give(ctx):
     member = ctx.author
     guild = ctx.guild
     role = guild.get_role(1232416938404085910)
     await member.add_roles(role)
-bot.run(TOKEN)
+    
+bot.run(config.token)
