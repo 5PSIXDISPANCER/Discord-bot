4eccf6a4ba6756b8100558a67ff36a8b5c161ccc
MRK <MRQ>
2024-05-15 21:31:19 +0300

    Add logging function

--- osnova.py
+++ osnova.py
@@ -1,8 +1,11 @@
 import discord
 import pytube
+import openpyxl
 
 import config
 
+from datetime import datetime
+from openpyxl import Workbook
 from discord.voice_client import VoiceClient
 from discord.ext import commands
 from discord.utils import get
@@ -11,15 +14,44 @@ intents = discord.Intents().all()
 
 bot = commands.Bot(command_prefix=config.prefix, intents=intents)
 
+async def log(message):
+    with open('log.txt', 'a') as file:
+        file.write(f'{datetime.now().strftime("%H:%M:%S")} {message.author.name}: {message.content}\n')
+        file.close()
+
+@bot.event
+async def on_message(message: discord.Message):
+    await bot.process_commands(message)
+    await log(message)
+
+@bot.command()
+async def logs(ctx):
+    print('work')
+    await ctx.send(file=discord.File(r'log.txt'))
+
 @bot.command()
 async def ping(stx):
+    book = Workbook()
+    sheep = book.active
+    mainRow = 1
+
     for i in range(len(bot.guilds)):
-        await stx.send(f'Сервер {bot.guilds[i]} на нем находятся:')
+        sheep.cell(row = mainRow, column = 1, value = bot.guilds[i].name)
         for x in range(len(bot.guilds[i].members)):
-            await stx.send(bot.guilds[i].members[x].name)
+            sheep.cell(row = mainRow, column = 2, value = bot.guilds[i].members[x].name)
+            sheep.cell(row = mainRow, column = 3, value = bot.guilds[i].members[x].global_name)
+            if bot.guilds[i].members[x].avatar == None:
+                sheep.cell(row = mainRow, column = 4, value = bot.guilds[i].members[x].default_avatar.url)
+            else:
+                sheep.cell(row = mainRow, column = 4, value = bot.guilds[i].members[x].avatar.url)
+            mainRow += 1
+    book.save('parser.xlsx')
+    book.close
+    await stx.send(file = discord.File(r'parser.xlsx'))
+
 @bot.command()
 async def poshel(stx):
-    await stx.send('nahuy')
+    await stx.send(bot.guilds[0].members[0].default_avatar)
 
 # Выдать роль
 @bot.command()
@@ -32,7 +64,7 @@ async def give(ctx,rolename, member: discord.Member = None):
 
 # Убрать роль
 @bot.command()
-async def remove(ctx,rolename, member: discord.Member = None):
+async def remove(ctx, rolename, member: discord.Member = None):
     if member is None:
         member = ctx.author
     role = get(ctx.guild.roles, name=rolename)
@@ -55,7 +87,7 @@ async def remove(ctx,rolename, member: discord.Member = None):
 
 @bot.command()
 async def gel(ctx, amount = 100):
-     await ctx.channel.purge(limit = amount);
+     await ctx.channel.purge(limit = amount)
 
 @bot.event
 async def on_ready():
