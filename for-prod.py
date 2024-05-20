ca4c9db933984b57d0bbafcf99529b31f7600832
Ben-Beee <yepupuyp@mail.ru>
2024-05-20 20:05:07 +0300

    dd

--- for-prod.py
+++ for-prod.py
@@ -16,4 +16,12 @@
 #     user_1 = guild.get_member(config.soft_devolopment_dima) 
 #     user_2 = guild.get_member(config.soft_devolopment_kirill)
 #     await channel.send(f"Пидорасы, что меня разрабатывают и используют {user_1.mention} и {user_2.mention}")
-# внесение в конфиг soft_devolopment_dima = 
+# внесение в конфиг soft_devolopment_dima = и тп
+# Функция для записи в текствой файл даты и времени, автора с id и текстом сообщения
+# from datetime import datetime
+# @bot.event
+# async def on_message(ctx):
+#     f = open("Logs.txt", "a")
+#     now = datetime.now()
+#     f.write(f"Дата: {now.strftime("%d/%m/%Y")} Время: {now.strftime("%H:%M:%S")} Автор: {ctx.author} ({ctx.author.id}) Сообщение: {ctx.content}\n")
+#     f.close()
