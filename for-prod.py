54b8445b93eb6662a3f3545426baa1e4e796effc
Ben-Beee <yepupuyp@mail.ru>
2024-05-20 12:12:52 +0300

    dd

---
+++ for-prod.py
@@ -0,0 +1,8 @@
+# Функция удаления сообщений с аргументом в виде скольких сообщений.
+# import asyncio
+# @bot.command()
+# async def delete(ctx, content: int ):
+#         await ctx.channel.purge(limit=content)
+#         await ctx.send('Так нахуй, этот уебан удалил, ДА ДА ОН! {}'.format(ctx.author.mention))
+#         await asyncio.sleep(5)
+#         await ctx.channel.purge(limit=1)
