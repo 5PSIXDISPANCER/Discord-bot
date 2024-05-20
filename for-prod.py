52d20c1257a332a548edbe7e4aeb6dfb6db944fb
Ben-Beee <yepupuyp@mail.ru>
2024-05-20 21:22:16 +0300

    dd

--- for-prod.py
+++ for-prod.py
@@ -20,10 +20,10 @@
 # Функция для записи в текствой файл даты и времени, автора с id и текстом сообщения + запись в канал
 # @bot.event
 # async def on_message(ctx):
-#     f = open("Logs.txt", "a")
-#     now = datetime.now()
-#     f.write(f"Дата: {now.strftime("%d/%m/%Y")} Время: {now.strftime("%H:%M:%S")} Автор: {ctx.author} ({ctx.author.id}) Категория: {ctx.channel.category} ({ctx.channel.category.id}) Канал: {ctx.channel} ({ctx.channel.id}) Сообщение: {ctx.content}\n")
-#     f.close()
-#     channel = bot.get_channel(config.channel)
 #     if ctx.author != bot.user:
-#         await channel.send(f"Дата: {now.strftime("%d/%m/%Y")} Время: {now.strftime("%H:%M:%S")} Автор: {ctx.author} ({ctx.author.id}) Категория: {ctx.channel.category} ({ctx.channel.category.id}) Канал: {ctx.channel} ({ctx.channel.id}) Сообщение: {ctx.content}\n")
+#         f = open("Logs.txt", "a")
+#         now = datetime.now()
+#         f.write(f"Дата: {now.strftime("%d/%m/%Y")} Время: {now.strftime("%H:%M:%S")} Автор: {ctx.author} ({ctx.author.id}) Категория: {ctx.channel.category} ({ctx.channel.category.id}) Канал: {ctx.channel} ({ctx.channel.id}) Сообщение: {ctx.content}\n")
+#         f.close()
+#         channel = bot.get_channel(config.channel)
+#         await channel.send(f"Дата: {now.strftime("%d/%m/%Y")} Время: {now.strftime("%H:%M:%S")} Автор: {ctx.author.mention} ({ctx.author.id}) Категория: {ctx.channel.category} ({ctx.channel.category.id}) Канал: {ctx.channel.mention} ({ctx.channel.id}) Сообщение: {ctx.jump_url}\n")
