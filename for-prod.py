d591906f170d84395b11d473cc6cf33bee39b7a9
MRK <MRQ>
2024-05-29 00:25:19 +0300

    this is chisto

--- for-prod.py
+++ for-prod.py
@@ -10,15 +10,6 @@
 #     channel = ctx.message.author.voice.channel
 #     await channel.connect()
 
-
-# Функция удаления сообщений с аргументом в виде скольких сообщений будем дорабатоваться.
-# import asyncio
-# @bot.command()
-# async def delete(ctx, content: int ):
-#         await ctx.channel.purge(limit=content)
-#         await ctx.send('Так нахуй, этот уебан удалил, ДА ДА ОН! {}'.format(ctx.author.mention))
-#         await asyncio.sleep(5)
-#         await ctx.channel.purge(limit=1)
 # Хуйня о запуске, можно еще что-то добавить 
 # @bot.event
 # async def on_ready():
